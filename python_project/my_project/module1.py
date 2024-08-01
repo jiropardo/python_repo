01376c9a5c624e3a1e2f4d6e1a08dc47f9e55363
jiropardo <jairo.prado@databricks.com>
2024-07-31 22:30:18 -0600

    ep

--- python_project/my_project/module1.py
+++ python_project/my_project/module1.py
@@ -1,7 +1,10 @@
 class MyClass:
     def __init__(self):
         print("MyClass instance created")
-        
+
+    def method_ONE():
+        return "Class function"    
+
 def function1():
         return "Hello from module1!"
 
