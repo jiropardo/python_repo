4d1c85db1f2185ef94cd344fa65b882b008deb98
jiropardo <54973723+jiropardo@users.noreply.github.com>
2025-01-08 11:03:05 -0600

    Update test_module1.py

--- python_project/tests/test_module1.py
+++ python_project/tests/test_module1.py
@@ -4,6 +4,8 @@ from my_project.module1 import function1
 
 class TestModule1(unittest.TestCase):
     def test_function1(self):
+        result = function1()
++       print(result)
         self.assertEqual(function1(), "Hello from module1!")
 
 if __name__ == "__main__":
